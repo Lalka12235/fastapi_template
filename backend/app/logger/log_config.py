c9e54f08c1dd44b1f709ab8604e65d4c359fd498
Egor <fisher12235@mail.ru>
2025-04-29 17:04:33 +0700

    fix

--- backend/app/logger/log_config.py
+++ backend/app/logger/log_config.py
@@ -1,23 +1,23 @@
 import logging
-import sys
 from logging.handlers import RotatingFileHandler
+import os
 
 def configure_logging(level=logging.INFO, log_file='app/logger/logs/app.log'):
+    # Убедимся, что директория существует
+    os.makedirs(os.path.dirname(log_file), exist_ok=True)
+
     formatter = logging.Formatter(
         fmt='[%(asctime)s] %(name)s:%(lineno)d %(levelname)-7s - %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S'
     )
 
-    #console_handler = logging.StreamHandler(sys.stdout)
-    #console_handler.setFormatter(formatter)
-
-    file_handler = RotatingFileHandler(log_file) #, maxBytes=5*1024*1024, backupCount=3
+    # Очищаем лог при запуске
+    file_handler = RotatingFileHandler(log_file, mode='w', maxBytes=5 * 1024 * 1024, backupCount=3)
     file_handler.setFormatter(formatter)
 
     root_logger = logging.getLogger()
     root_logger.setLevel(level)
 
-
-    if not root_logger.handlers:
-        #root_logger.addHandler(console_handler)
-        root_logger.addHandler(file_handler)
+    root_logger.handlers.clear()
+    
+    root_logger.addHandler(file_handler)
