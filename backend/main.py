16573ebfcca0ae2e7f1d4296801b8248a56bc028
Egor <fisher12235@mail.ru>
2025-05-08 18:18:50 +0700

    fix

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import logging
+from app.logger.log_config import configure_logging
+
+configure_logging()
+logging.getLogger("watchfiles").setLevel(logging.WARNING)
+
 
 app = FastAPI()
+
+
+app = FastAPI(
+    title='FastAPI Template',
+    description='Just use in your project'
+)
+
+@app.get('/ping')
+async def ping():
+    return 'Server is running'
 
 
 origins = [
